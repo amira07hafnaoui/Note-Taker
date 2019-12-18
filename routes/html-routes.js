9855afbd29f06cef129f5d8974e6662549cb9d0d
amira <chikhaoui.amira07@gmail.com>
2019-12-17 21:02:34 -0600

    note-taker

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,12 @@
+
 var path = require("path");
-// Routes
-// module.exports = function(app) {
-//     // return notes.html file
-//     app.get("/notes", function(req, res) {
-//         res.sendFile(path.join(`${__dirname}/../public`, "notes.html"));
-//     });
-
-//     app.get("*", function(req, res) {
-//         res.sendFile(path.join(`${__dirname}/../public`, "index.html"));
-//     });
-
-// }
-
+// configuring routes for HTTP requests.
 module.exports = function (app) {
 // return notes.html file
     app.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-
+// return index.html file
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
--- server.js
+++ server.js
@@ -2,26 +2,24 @@
 // Series of npm packages that we will use to give our server useful functionality
 const path = require("path");
 var express = require("express");
+// create express app
 var app = express();
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port. We"ll use this in our listener
 var PORT = process.env.PORT || 3030;
-
-let noteCount = 1;
+ let noteCount = 1;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-// configuring routes for HTTP requests.
+
+// create a controller for our note app so we can handle the routes and the rendering of
+// views then require the controllers (modules)
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
-// Start the server & listen to incoming requests
+// Start the server & listen for requests
 app.listen(PORT, function() {
     console.log('server is running and listening on http://localhost:' + PORT)
 });

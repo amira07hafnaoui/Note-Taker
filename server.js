96af50d6ae8e838bead1d092c567b0b1529bcac0
amira07hafnaoui <55209230+amira07hafnaoui@users.noreply.github.com>
2019-12-17 21:07:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // static files
 app.use(express.static(path.join(__dirname, "public")));
-
-
 // create a controller for our note app so we can handle the routes and the rendering of
 // views then require the controllers (modules)
 require("./routes/api-routes")(app);
